66c8cb8a36832d6ebacd86e8228b3dd048ad4d1c
passSzj <y462477976@outlook.com>
2025-08-08 15:31:24 +0800

    feat:基于overlayfs实现容器内文件的隔离

--- container/container_process.go
+++ container/container_process.go
@@ -1,9 +1,11 @@
 package container
 
 import (
+	"fmt"
 	log "github.com/sirupsen/logrus"
 	"os"
 	"os/exec"
+	"path"
 	"syscall"
 )
 
@@ -40,6 +42,109 @@ func NewParentProcess(tty bool) (*exec.Cmd, *os.File) {
 		cmd.Stderr = os.Stderr
 	}
 	cmd.ExtraFiles = []*os.File{readPipe} // 将读取方转入子进程
-	cmd.Dir = "/root/busybox"             // 设置工作目录为 /root/busybox
+	rootPath := "/root"
+	NewWorkSpace(rootPath) // 设置工作目录为 /root/busybox
+	cmd.Dir = path.Join(rootPath, "merged")
 	return cmd, writePipe
+}
+
+// NewWorkSpace Create an Overlay2 filesystem as container root workspace
+func NewWorkSpace(rootPath string) {
+	createLower(rootPath)
+	createDirs(rootPath)
+	mountOverlayFS(rootPath)
+}
+
+// createLower 将busybox作为overlayfs的lower层
+func createLower(rootPath string) {
+	// 把busybox作为overlayfs中的lower层
+	busyboxPath := path.Join(rootPath, "busybox")
+	busyboxTarPath := path.Join(rootPath, "busybox.tar")
+	log.Infof("busybox:%s busybox.tar:%s", busyboxPath, busyboxTarPath)
+	// 检查是否已经存在busybox文件夹
+	exist, err := PathExists(busyboxPath)
+	if err != nil {
+		log.Infof("Fail to judge whether dir %s exists. %v", busyboxPath, err)
+	}
+	// 不存在则创建目录并将busybox.tar解压到busybox文件夹中
+	if !exist {
+		if err = os.Mkdir(busyboxPath, 0777); err != nil {
+			log.Errorf("Mkdir dir %s error. %v", busyboxPath, err)
+		}
+		if _, err = exec.Command("tar", "-xvf", busyboxTarPath, "-C", busyboxPath).CombinedOutput(); err != nil {
+			log.Errorf("Untar dir %s error %v", busyboxPath, err)
+		}
+	}
+}
+
+// createDirs 创建overlayfs需要的的merged、upper、worker目录
+func createDirs(rootPath string) {
+	dirs := []string{
+		path.Join(rootPath, "merged"),
+		path.Join(rootPath, "upper"),
+		path.Join(rootPath, "work"),
+	}
+
+	for _, dir := range dirs {
+		if err := os.Mkdir(dir, 0777); err != nil {
+			log.Errorf("mkdir dir %s error. %v", dir, err)
+		}
+	}
+}
+
+// mountOverlayFS 挂载overlayfs
+func mountOverlayFS(rootPath string) {
+	// 拼接参数
+	// e.g. lowerdir=/root/busybox,upperdir=/root/upper,workdir=/root/work
+	dirs := fmt.Sprintf("lowerdir=%s,upperdir=%s,workdir=%s", path.Join(rootPath, "busybox"),
+		path.Join(rootPath, "upper"), path.Join(rootPath, "work"))
+
+	// 完整命令：mount -t overlay overlay -o lowerdir=/root/busybox,upperdir=/root/upper,workdir=/root/work /root/merged
+	cmd := exec.Command("mount", "-t", "overlay", "overlay", "-o", dirs, path.Join(rootPath, "merged"))
+	log.Infof("mount overlayfs: [%s]", cmd.String())
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Errorf("%v", err)
+	}
+}
+
+// DeleteWorkSpace Delete the AUFS filesystem while container exit
+func DeleteWorkSpace(rootPath string) {
+	umountOverlayFS(path.Join(rootPath, "merged"))
+	deleteDirs(rootPath)
+}
+
+func umountOverlayFS(mntPath string) {
+	cmd := exec.Command("umount", mntPath)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Errorf("%v", err)
+	}
+}
+
+func deleteDirs(rootPath string) {
+	dirs := []string{
+		path.Join(rootPath, "merged"),
+		path.Join(rootPath, "upper"),
+		path.Join(rootPath, "work"),
+	}
+
+	for _, dir := range dirs {
+		if err := os.RemoveAll(dir); err != nil {
+			log.Errorf("Remove dir %s error %v", dir, err)
+		}
+	}
+}
+
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
 }
